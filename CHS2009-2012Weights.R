d38c3e3dd9743108e321a59519c44ccf33c6ecd8
audreyrenson <audrey.o.renson@gmail.com>
2015-09-07 13:23:34 -0400

    R survey weights script

--- CHS2009-2012Weights.R
+++ CHS2009-2012Weights.R
@@ -13,3 +13,11 @@ library(sas7bdat)
 chsWeights <- read.sas7bdat("/users/audreyrenson/google drive/NYC CHS Healtcare Access Project/Survey Weights 2009-2012/chs2009_2012combinedweights.sas7bdat")
 attach(chsWeights)
 
+setwd("/Users/audreyrenson/CHS-4-year-weighted-data/data-files/")
+chs2009 <- read.sas7bdat("chs2009_public.sas7bdat")
+chs2010 <- read.sas7bdat("chs2010_public.sas7bdat")
+chs2011 <- read.sas7bdat("chs2011_public.sas7bdat")
+chs2012 <- read.sas7bdat("chs2012_public.sas7bdat")
+
+
+
